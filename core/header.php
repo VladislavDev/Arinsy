93613bb4a3dcdf718689b106588498c5d6aff389
Sokolov Vladislav <Vladsok1998@mail.ru>
2019-05-29 00:05:17 +0700

    Work by header start

--- core/header.php
+++ core/header.php
@@ -1,11 +1,34 @@
 <?php
-    include_once $_SERVER["DOCUMENT_ROOT"]."/Arincy/core/prolog.php";
+    //prolog file including
+    include_once $_SERVER["DOCUMENT_ROOT"]."/Arinsy/core/prolog.php";
 ?>
 
 <html>
     <head>
+        <title>Arinsy</title>
+
+        <meta charset="utf-8">
+        <meta name="description" content="CRM for your business">
+        <meta name="keywords" lang="en" content="CRM, business-process">
+
+        <!-- including of main css files -->
+        <?php
+            //getting a list of files
+            $arCSSFiles = scandir(MAIN_STYLES);
+            //including each file from the list
+			for ($i = 2; $i < count($arCSSFiles); $i++){
+				echo '<link rel="stylesheet" href="css/'.$arCSSFiles[$i].'" type="text/css"/>';
+            }
+		?>
 
     </head>
     <body>
+        <!-- the block caps of the website -->
         <div class="header">
+            <div class="logo-container">
+                <img
+                    src="content/main/logo.png"
+                    class="img_in_container"
+                />
+            </div>
         </div>
--- core/settings.php
+++ core/settings.php
@@ -8,11 +8,11 @@
     */
 
     //                               *** Folders ***
-    define('SITE_NAME',         $_SERVER["DOCUMENT_ROOT"]."/Arinsy/");      //Domain of site
-    define('MAIN_STYLES',       SITE_NAME."css/");                      //Main styles dir
-    define('MAIN_JS',           SITE_NAME."js/");                       //Main js-scripts dir
-    define('CONTENT_DIR',       SITE_NAME."content/");                  //Content dir
-    define('MAIN_CONTENT',      SITE_NAME."main/");                     //Main content of site
+    define('SITE_NAME',         $_SERVER["DOCUMENT_ROOT"]."/Arinsy/");                  //Domain of site
+    define('MAIN_STYLES',       SITE_NAME."css/");                                      //Main styles dir
+    define('MAIN_JS',           SITE_NAME."js/");                                       //Main js-scripts dir
+    define('CONTENT_DIR',       SITE_NAME."content/");                                  //Content dir
+    define('MAIN_CONTENT',      CONTENT_DIR."main/");                                   //Main content of site
 
     //                                *** Files ***
 ?>
