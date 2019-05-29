2491b4cd0f1a32a661d582c86315cbc168d9bef5
Sokolov Vladislav <Vladsok1998@mail.ru>
2019-05-29 14:59:55 +0700

    Create LogIn Form without buttons

---
+++ core/footer.php
@@ -0,0 +1,8 @@
+        <div class="footer">
+        </div>
+    </body>
+</html>
+
+<?php
+    include_once $_SERVER["DOCUMENT_ROOT"]."/Arinsy/core/epilog.php";
+?>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     include $_SERVER["DOCUMENT_ROOT"]."/Arinsy/core/header.php";
 ?>
+
+
 <div class="info-body">
     <div class="about_a_project">
         <h1>Arinsy</h1>
@@ -19,7 +21,31 @@
         </div>
         <h3 class="blue">Project support</h3>
         <div class="AAP_info">
-            Now the repository is hosted on GitHub, and we welcome any comments and pull-requests.
+            Now the <a href="https://github.com/VladislavDev/Arinsy">repository</a> is hosted 
+            on GitHub, and we welcome any comments and pull-requests.
         </div>
     </div>
 </div>
+<a href="#LogIn">
+    <div class="LogInButton">
+        Log In
+    </div>
+</a>
+<div class="LogInFormDialog" id="LogIn">
+    <div class="LogInFormBody">
+        <a href="#" title="Close" class="LogInFormClose">X</a>
+
+        <form method="POST" class="LogInForm">
+            <label for='.LogInForm' class="LIF_label">Enter your login and password:</label>
+            <br/>
+            <input type="text" class="LIF_input" name="Arinsy_Login" placeholder="Login or E-mail" required/>
+            <br/>
+            <input type="password" class="LIF_input" name="Arinsy_Pswd" placeholder="Password" required/>
+        </form>
+    </div>
+</div>
+
+
+<?php
+    include $_SERVER["DOCUMENT_ROOT"]."/Arinsy/core/footer.php";
+?>
