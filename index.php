604f6dcd711b495bbf7b01edc8c51146d7207bd2
Sokolov Vladislav <Vladsok1998@mail.ru>
2019-05-29 11:56:48 +0700

    Some information on start page

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
     include $_SERVER["DOCUMENT_ROOT"]."/Arinsy/core/header.php";
 ?>
+<div class="info-body">
+    <div class="about_a_project">
+        <h1>Arinsy</h1>
+        <div class="AAP_info">
+            This is an actively developed project. The aim of the project is to create a CRM with the 
+            ability to manage business processes, its own workflow engine and flexible management of created
+            tasks. With this system you will be able to create tasks and projects, realize the possibility 
+            of teamwork and partially automate the workflow.
+        </div>
+        <h3>Project development</h3>
+        <div class="AAP_info">
+            Now you will not be able to try out the functionality, because the project has just started to 
+            be developed. The first success and launch of the web-version is planned in early July. Later, 
+            it is planned to create a desktop-version and a mobile application, as well as build-up of 
+            functionality.
+        </div>
+        <h3 class="blue">Project support</h3>
+        <div class="AAP_info">
+            Now the repository is hosted on GitHub, and we welcome any comments and pull-requests.
+        </div>
+    </div>
+</div>
